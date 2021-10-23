e9314be2524a4c577fbd4aa816f3ebc27c7d9d80
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-10-23 21:46:03 +0530

    added div

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,6 @@
-<div class="auth"><h5>Designed by <a  href="https://ruchiras.com" target="_blank">RUCHIRA</a></h5></div>
+<div class="auth">
+    <h5>Designed by <a  href="https://ruchiras.com" target="_blank">RUCHIRA</a></h5>
+</div>
     </div>
 </body>
 </html>
